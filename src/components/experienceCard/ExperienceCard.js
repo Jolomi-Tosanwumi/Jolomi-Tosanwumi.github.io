ab14fb4430ab55db3c896807a23f88ac13e463f1
Jolomi-Tosanwumi <tjolomi@gmail.com>
2024-10-13 08:06:50 -0400

    fixed bug

--- src/components/experienceCard/ExperienceCard.js
+++ src/components/experienceCard/ExperienceCard.js
@@ -33,7 +33,7 @@ export default function ExperienceCard({cardInfo, isDark}) {
 
   return (
     <div className={isDark ? "experience-card-dark" : "experience-card"}>
-      <Fade right duration={1000}>
+      <Fade bottom duration={1000}>
         <div style={{background: rgb(colorArrays)}} className="experience-banner">
           <div className="experience-blurred_div"></div>
           <div className="experience-div-company">
